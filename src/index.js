66bfa60220221b694f9ea5f1c9c218f163355c19
plm9606 <plm9606@naver.com>
2019-12-25 22:09:58 +0900

    chore: context.repo 출력 문구 추가

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  console.log(`context.repo: \n${github.context.repo}`);
 } catch (error) {
   core.setFailed(error.message);
 }
