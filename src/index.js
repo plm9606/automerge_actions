03e0bc48279737f1f8a3b2fa93c3eedf26a4b9b1
aram <plm9606@naver.com>
2020-05-08 22:00:21 +0900

    Add auto delete option (#7)
    * chore: remove node_modules
    
    * feat: add branch auto delete options

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ async function autoMerge() {
     const labelName = core.getInput("label-name");
     const reviewersNumber = core.getInput("reviewers-number");
     const merge_method = core.getInput("merge-method");
+    const autoDelete = core.getInput("auto-input");
     const myToken = core.getInput("github-token");
     const octokit = new github.GitHub(myToken);
 
@@ -16,26 +17,34 @@ async function autoMerge() {
 
     const reviews = await octokit.pulls.listReviews({
       ...github.context.repo,
-      pull_number
+      pull_number,
     });
     const pr = await octokit.pulls.get({
       ...github.context.repo,
-      pull_number
+      pull_number,
     });
 
     const labels = pr.data.labels;
-    const hasAutomerge = labels.some(label => label.name === labelName);
+    const hasAutomerge = labels.some((label) => label.name === labelName);
 
     if (hasAutomerge) {
       if (reviews.data.length >= +reviewersNumber) {
         octokit.pulls.merge({
           ...github.context.repo,
           pull_number,
-          merge_method
+          merge_method,
         });
         core.info(`Success Merge PR!`);
       } else throw Error("You need to get other's review!");
     } else core.info(`You don't labeled auto merge label::"${labelName}"`);
+
+    if (autoDelete.toUpperCase() === "TRUE") {
+      core.info("The Bot will remove merged branch.");
+      octokit.git.deleteRef({
+        ...github.context.repo,
+        ref,
+      });
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
